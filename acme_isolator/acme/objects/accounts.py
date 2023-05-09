c5ecaf61e03df54cd71b76973b1927755648330e
Niclas Kretschmer <niclas.kretschmer@protonmail.com>
2023-05-09 05:13:32 +0200

    Order list added

--- acme_isolator/acme/objects/accounts.py
+++ acme_isolator/acme/objects/accounts.py
@@ -1,5 +1,6 @@
 from .base import ACME_Object
 from dataclasses import dataclass
+from .order import ACME_Orders
 
 
 @dataclass(order=False, kw_only=True)
@@ -7,4 +8,5 @@ class ACME_Account(ACME_Object):
     status: str
     contact: list[str]|None
     termsOfServiceAgreed: bool|None
-    orders: str
+    orders: str|ACME_Orders
+    url: str
--- acme_isolator/acme/objects/order.py
+++ acme_isolator/acme/objects/order.py
@@ -2,6 +2,7 @@ from .base import ACME_Object
 from .identifier import ACME_Identifier
 from .authorization import ACME_Authorization
 from dataclasses import dataclass, field, InitVar
+from collections.abc import Sequence
 
 
 @dataclass(order=False, kw_only=True)
@@ -16,6 +17,22 @@ class ACME_Order(ACME_Object):
     authorizations: list[str]|list[ACME_Authorization]
     finalize: str
     certificate: str|None
+    url: str
 
     def __post_init__(self, identifier: dict):
         self.identifier = ACME_Identifier.parse(identifier)
+
+
+@dataclass(order=False, kw_only=True)
+class ACME_Orders(ACME_Object, Sequence):
+    orders: list[str]|list[ACME_Order]
+    url: str
+
+    def __getitem__(self, i):
+        return self.orders[i]
+
+    def __len__(self):
+        return len(self.orders)
+
+    def __iter__(self):
+        return self.orders.__iter__()
