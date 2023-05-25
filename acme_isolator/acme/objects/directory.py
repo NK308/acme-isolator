5f50485ed71ab8266a38b8dc82952583a85d5eb4
Niclas Kretschmer <niclas.kretschmer@protonmail.com>
2023-05-25 09:03:11 +0200

    Requesting directory resource refactored

--- acme_isolator/acme/objects/directory.py
+++ acme_isolator/acme/objects/directory.py
@@ -1,5 +1,7 @@
 from .base import ACME_Object
 from dataclasses import dataclass, InitVar, field
+from aiohttp import request
+from ..request.constants import USER_AGENT
 
 
 @dataclass(order=False, kw_only=True)
@@ -15,4 +17,15 @@ class ACME_Directory(ACME_Object):
 
     def __post_init__(self, meta):
         self.website = meta["website"]
+
+    def __iter__(self):
+        return {k: v for (k,v) in self.__dict__ if k in {"newNonce", "newAccount", "newOrder", "newAuthz", "revoceCert", "keyChange"}}
+
+    @classmethod
+    async def get_directory(cls, directory_url: str):
+        async with request("GET", directory_url, headers={"User-Agent": USER_AGENT}) as resp:
+            resp.raise_for_status()
+            j = await resp.json(encoding="utf-8")
+            return cls(**j)
+
 
