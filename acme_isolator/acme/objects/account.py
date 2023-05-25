0161b0f7c4c1a2bf6537fce8fa09840ebe36ce07
Niclas Kretschmer <niclas.kretschmer@protonmail.com>
2023-05-25 09:05:09 +0200

    Stub methods for creating/getting account objects added

--- acme_isolator/acme/objects/account.py
+++ acme_isolator/acme/objects/account.py
@@ -1,6 +1,8 @@
 from .base import ACME_Object
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from .order import ACME_Orders
+from cryptography.hazmat.primitives.asymmetric.ec import EllipticCurvePrivateKey
+from ..request import JwsKid, JwsJwk
 
 
 @dataclass(order=False, kw_only=True)
@@ -9,3 +11,13 @@ class ACME_Account(ACME_Object):
     contact: list[str] | None
     termsOfServiceAgreed: bool | None
     orders: str | ACME_Orders
+    resources: dict[str, str]
+    parent: None = field(default=None, init=False)
+
+    @classmethod
+    async def create_from_key(cls, url: str, key: EllipticCurvePrivateKey):
+        pass
+
+    @classmethod
+    async def get_from_key(cls, url: str, key: EllipticCurvePrivateKey):
+        pass
