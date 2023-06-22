db309e17eb0837cf927a7dedd331617d84fedebd
Niclas Kretschmer <niclas.kretschmer@protonmail.com>
2023-06-22 08:22:16 +0200

    ACME_Challenge: More stub

--- acme_isolator/acme/objects/challenge.py
+++ acme_isolator/acme/objects/challenge.py
@@ -5,6 +5,18 @@ from dataclasses import dataclass
 
 @dataclass
 class ACME_Challenge(ABC, ACME_Object):
+    type: str
+    status: str
+    validated: None | str
+    error: None | dict | list | str
     # For now this is just a stub
     #TODO add concrete implementation for dns challenge
-    pass
+
+    def keyAuthorization(self) -> str:
+        #TODO implement rfc7638
+        pass
+
+
+class ACME_Challenge_dns_01(ACME_Challenge):
+    token: str
+    type: str = "dns-01"
