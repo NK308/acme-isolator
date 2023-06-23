d32f4bcec442f60e02eb1865746f860c16b3c1da
Niclas Kretschmer <niclas.kretschmer@protonmail.com>
2023-06-23 14:58:38 +0200

    Package resolution fixed

--- acme_isolator/acme/objects/__init__.py
+++ acme_isolator/acme/objects/__init__.py
@@ -1,5 +1,5 @@
 from .directory import ACME_Directory
 from .authorization import ACME_Authorization
-from challenge import ACME_Challenge
+from .challenge import ACME_Challenge
 from .order import ACME_Orders, ACME_Order
 from .account import ACME_Account
