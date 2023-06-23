47b93e40532e39c1d4b8b93478e9702be95f3d27
Niclas Kretschmer <niclas.kretschmer@protonmail.com>
2023-06-23 18:43:02 +0200

    First unit test added

--- tests/acme/test_directory.py
+++ tests/acme/test_directory.py
@@ -1,15 +1,7 @@
 import pytest
 import aiohttp
-
-class MockDirectoryResourceResponse:
-
-    @staticmethod
-    def raise_for_status():
-        return ""
-
-    @staticmethod
-    async def json(*args, **kwargs):
-        return { # json stolen from the LetsEncrypt staging environment
+from acme_isolator.acme.objects.directory import ACME_Directory
+directory_resource_json = { # json stolen from the LetsEncrypt staging environment
             "NW9luPYOK8w": "https://community.letsencrypt.org/t/adding-random-entries-to-the-directory/33417",
             "keyChange": "https://acme-staging-v02.api.letsencrypt.org/acme/key-change",
             "meta": {
@@ -26,9 +18,40 @@ class MockDirectoryResourceResponse:
             "revokeCert": "https://acme-staging-v02.api.letsencrypt.org/acme/revoke-cert"
         }
 
+class MockDirectoryResourceResponse:
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    @staticmethod
+    def raise_for_status():
+        return ""
+
+    @staticmethod
+    async def json(*args, **kwargs):
+        return directory_resource_json
+
 
 @pytest.fixture()
 def mock_directory_resource(monkeypatch):
-    async def get_mock_response():
+    async def get_mock_response(*args, **kwargs):
         return MockDirectoryResourceResponse()
-    monkeypatch.setattr(aiohttp, "request", mock_directory_resource)
+    monkeypatch.setattr("aiohttp.request", get_mock_response)
+
+
+@pytest.mark.asyncio
+async def test_directory_parsing(mock_directory_resource):
+    d = await ACME_Directory.get_directory("https://acme-staging-v02.api.letsencrypt.org/directory")
+    assert d.newNonce == directory_resource_json["newNonce"]
+    assert d.newAccount == directory_resource_json["newAccount"]
+
+
+@pytest.mark.asyncio
+async def test_directory_fetching():
+    d = await ACME_Directory.get_directory("https://acme-staging-v02.api.letsencrypt.org/directory")
+    assert d.newNonce == directory_resource_json["newNonce"]
+    assert d.newAccount == directory_resource_json["newAccount"]
+    assert type(d) == ACME_Directory
