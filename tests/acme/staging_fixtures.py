02fd4eecaa0a354b104a66488e5a21de1713177b
Niclas Kretschmer <niclas.kretschmer@protonmail.com>
2023-07-12 01:41:35 +0200

    Hardcoded staging directory json moved

--- tests/acme/staging_fixtures.py
+++ tests/acme/staging_fixtures.py
@@ -2,6 +2,22 @@ import pytest
 import pytest_asyncio
 from aiohttp import ClientSession
 
+directory_resource_json = { # json stolen from the LetsEncrypt staging environment
+    "keyChange": "https://acme-staging-v02.api.letsencrypt.org/acme/key-change",
+    "meta": {
+        "caaIdentities": [
+            "letsencrypt.org"
+        ],
+        "termsOfService": "https://letsencrypt.org/documents/LE-SA-v1.3-September-21-2022.pdf",
+        "website": "https://letsencrypt.org/docs/staging-environment/"
+    },
+    "newAccount": "https://acme-staging-v02.api.letsencrypt.org/acme/new-acct",
+    "newNonce": "https://acme-staging-v02.api.letsencrypt.org/acme/new-nonce",
+    "newOrder": "https://acme-staging-v02.api.letsencrypt.org/acme/new-order",
+    "renewalInfo": "https://acme-staging-v02.api.letsencrypt.org/draft-ietf-acme-ari-01/renewalInfo/",
+    "revokeCert": "https://acme-staging-v02.api.letsencrypt.org/acme/revoke-cert"
+}
+
 
 @pytest_asyncio.fixture()
 async def session(event_loop):
--- tests/acme/test_directory.py
+++ tests/acme/test_directory.py
@@ -1,21 +1,7 @@
 import pytest
 from acme_isolator.acme.objects.directory import ACME_Directory
-directory_resource_json = { # json stolen from the LetsEncrypt staging environment
-            "NW9luPYOK8w": "https://community.letsencrypt.org/t/adding-random-entries-to-the-directory/33417",
-            "keyChange": "https://acme-staging-v02.api.letsencrypt.org/acme/key-change",
-            "meta": {
-                "caaIdentities": [
-                    "letsencrypt.org"
-                ],
-                "termsOfService": "https://letsencrypt.org/documents/LE-SA-v1.3-September-21-2022.pdf",
-                "website": "https://letsencrypt.org/docs/staging-environment/"
-            },
-            "newAccount": "https://acme-staging-v02.api.letsencrypt.org/acme/new-acct",
-            "newNonce": "https://acme-staging-v02.api.letsencrypt.org/acme/new-nonce",
-            "newOrder": "https://acme-staging-v02.api.letsencrypt.org/acme/new-order",
-            "renewalInfo": "https://acme-staging-v02.api.letsencrypt.org/draft-ietf-acme-ari-01/renewalInfo/",
-            "revokeCert": "https://acme-staging-v02.api.letsencrypt.org/acme/revoke-cert"
-        }
+from .staging_fixtures import directory_resource_json
+
 
 class MockDirectoryResourceResponse:
 
