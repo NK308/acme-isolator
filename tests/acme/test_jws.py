4357a39350d51604a175870d3ad8ed7f868486d4
Niclas Kretschmer <niclas.kretschmer@protonmail.com>
2023-09-13 08:34:31 +0200

    Not required jws unit tests removed

--- tests/acme/test_jws.py
+++ tests/acme/test_jws.py
@@ -1,22 +1,6 @@
 from acme_isolator.acme.request import jws
 
 
-def test_string2utf8():
-    test_string = '{"typ":"JWT",\r\n "alg":"HS256"}' # example taken from rfc7515 A.1.1
-    result_list = [123, 34, 116, 121, 112, 34, 58, 34,
-                   74, 87, 84, 34, 44, 13, 10, 32,
-                   34, 97, 108, 103, 34, 58, 34, 72,
-                   83, 50, 53, 54, 34, 125]
-    assert [int(x) for x in jws.JwsBase._encode_string2utf8(test_string)] == result_list
-
-
-def test_utf82base64():
-    input_list = [123, 34, 116, 121, 112, 34, 58, 34,
-                   74, 87, 84, 34, 44, 13, 10, 32,
-                   34, 97, 108, 103, 34, 58, 34, 72,
-                   83, 50, 53, 54, 34, 125]
-    assert jws.JwsBase._encode_bytes2base64(bytes(input_list)) == "eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9"
-
 
 def test_jwk_bytes_constructor(generate_key_pair, random_payload, random_nonce):
     jws.JwsJwk(nonce=random_nonce, key=generate_key_pair, payload=random_payload, url="https://example.com/example_url")
