5ad965477c812544c273016d1597eecfb5a9eae2
Niclas Kretschmer <niclas.kretschmer@protonmail.com>
2024-01-23 19:32:31 +0100

    Order creation added

--- acme_isolator/acme/objects/order.py
+++ acme_isolator/acme/objects/order.py
@@ -1,4 +1,5 @@
 from .base import ACME_Object, ACME_List, ClassVar
+from .exceptions import UnexpectedResponseException
 from .identifier import ACME_Identifier
 from .authorization import ACME_Authorization
 from dataclasses import dataclass, field, InitVar
@@ -19,7 +20,7 @@ class ACME_Order(ACME_Object):
     certificate: str | None = None
 
     def __post_init__(self, identifiers: list[dict]):
-        self.identifier = [ACME_Identifier.parse(identifier) for identifier in identifiers]
+        self.identifiers = [ACME_Identifier.parse(identifier) for identifier in identifiers]
 
     async def update(self):  # TODO rewrite/remove
         data, status = await self.parent.parent.request(self.url, None)
@@ -38,4 +39,16 @@ class ACME_Orders(ACME_List[ACME_Order]):
     convert_table: ClassVar[dict] = {"orders": ACME_Order}
 
     orders: InitVar[list[dict]]
-    # TODO add method for creating new orders
+
+    async def create_order(self, identifiers: list[ACME_Identifier], notBefore: str | None = None, notAfter: str | None = None):
+        payload = {"notBefore": notBefore, "notAfter": notAfter}
+        payload["identifiers"] = [id.as_dict() for id in identifiers]
+        try:
+            resp, status, location = await self.account.post(self.account.session.directory.newOrder, payload=payload)
+            assert status == 201
+            resp.update({"url": location})
+            order = ACME_Order(parent=self, **resp)
+            self._list.append(order)
+            return order
+        except AssertionError:
+            raise UnexpectedResponseException(status, response=resp).convert_exception()
