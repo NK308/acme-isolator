f7793ef486ee6c480235b562a537b882c2d1eadb
Niclas Kretschmer <niclas.kretschmer@protonmail.com>
2024-01-23 19:29:48 +0100

    Identifier inheritance removed

--- acme_isolator/acme/objects/identifier.py
+++ acme_isolator/acme/objects/identifier.py
@@ -5,8 +5,10 @@ from typing import ClassVar
 
 _identifier_register = dict()
 
+
 @dataclass(order=False, kw_only=True)
-class ACME_Identifier(ACME_Object):
+class ACME_Identifier:
+
     type: ClassVar[str]
     value: str
 
@@ -17,11 +19,14 @@ class ACME_Identifier(ACME_Object):
     def __call__(self) -> str:
         return self.value
 
+    def as_dict(self) -> dict[str, str]:
+        return {"type": self.type, "value": self.value}
+
     @staticmethod
     def parse(identifier: dict) -> "ACME_Identifier":
         t = identifier.get("type", None)
         if t in _identifier_register.keys():
-            return _identifier_register[t](identifier)
+            return _identifier_register[t](value=identifier["value"])
         else:
             raise ValueError("Unknown/undefined identifier type")
 
