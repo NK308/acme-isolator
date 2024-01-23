996c829e0b59a54afb5738dfa7514d9131d5cf3f
Niclas Kretschmer <niclas.kretschmer@protonmail.com>
2024-01-23 16:04:09 +0100

    Parsing of identifiers refactored for subclasses

--- acme_isolator/acme/objects/identifier.py
+++ acme_isolator/acme/objects/identifier.py
@@ -1,24 +1,29 @@
 from abc import ABC
-from .base import ACME_Object
+from .base import ACME_Object, ElementList
 from dataclasses import dataclass, field
 from typing import ClassVar
 
+_identifier_register = dict()
 
 @dataclass(order=False, kw_only=True)
 class ACME_Identifier(ACME_Object):
     type: ClassVar[str]
     value: str
 
+    def __init_subclass__(cls, **kwargs):
+        global _identifier_register
+        _identifier_register[cls.type] = cls
+
     def __call__(self) -> str:
         return self.value
 
     @staticmethod
     def parse(identifier: dict) -> "ACME_Identifier":
-        if identifier["type"] == "dns":
-            return ACME_Identifier_DNS(**identifier)
+        t = identifier.get("type", None)
+        if t in _identifier_register.keys():
+            return _identifier_register[t](identifier)
         else:
             raise ValueError("Unknown/undefined identifier type")
-
 
 
 class ACME_Identifier_DNS(ACME_Identifier):
