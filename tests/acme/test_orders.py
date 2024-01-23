1995cb3839533a7b59b6a37a9d8b35a09630b1a3
Niclas Kretschmer <niclas.kretschmer@protonmail.com>
2024-01-23 19:32:57 +0100

    Test for order creation added

--- tests/acme/test_orders.py
+++ tests/acme/test_orders.py
@@ -2,7 +2,8 @@ import pytest
 import pytest_asyncio
 
 from acme_isolator.acme.objects.account import ACME_Account
-from acme_isolator.acme.objects.order import ACME_Orders, ACME_Order, ACME_Identifier
+from acme_isolator.acme.objects.order import ACME_Orders, ACME_Order
+from acme_isolator.acme.objects.identifier import ACME_Identifier_DNS
 
 
 class TestOrderCreation:
@@ -13,3 +14,11 @@ class TestOrderCreation:
         assert type(pebble_session.orders) == ACME_Orders.url_class
         pebble_session.orders = await pebble_session.orders.request_object(parent=pebble_session)
         assert type(pebble_session.orders) == ACME_Orders
+
+    @pytest.mark.pebble
+    @pytest.mark.asyncio
+    async def test_order_creation(self, pebble_session):
+        pebble_session.orders = await pebble_session.orders.request_object(parent=pebble_session)
+        identifiers = [ACME_Identifier_DNS(value="not-my.domain.com")]
+        order = await pebble_session.orders.create_order(identifiers=identifiers)
+        assert type(order) == ACME_Order
