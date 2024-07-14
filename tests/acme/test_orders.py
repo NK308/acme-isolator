0793a0e2b139e34bc8e24e1126be74c186351ea8
Niclas Kretschmer <niclas.kretschmer@protonmail.com>
2024-07-15 01:48:38 +0200

    Test, to check if authorizations of newly created orders are just URLs

--- tests/acme/test_orders.py
+++ tests/acme/test_orders.py
@@ -1,7 +1,7 @@
 import pytest
 import pytest_asyncio
 
-from acme_isolator.acme.objects.base import ElementList
+from acme_isolator.acme.objects.base import AcmeUrlBase, ElementList
 from acme_isolator.acme.objects.account import ACME_Account
 from acme_isolator.acme.objects.order import ACME_Orders, ACME_Order
 from acme_isolator.acme.objects.identifier import ACME_Identifier_DNS
@@ -46,4 +46,14 @@ class TestOrderCreation:
         assert type(orders._list) == list
         for order in orders:
             assert type(order) == ACME_Order
+
+    @pytest.mark.pebble
+    @pytest.mark.asyncio
+    async def test_authorization_datatype(self, pebble_session):
+        pebble_session.orders = await pebble_session.orders.request_object(parent=pebble_session)
+        identifiers = [ACME_Identifier_DNS(value="not-my.domain.com")]
+        order = await pebble_session.orders.create_order(identifiers=identifiers)
+        assert len(order.authorizations) > 0
+        for auth in order.authorizations:
+            assert isinstance(auth, AcmeUrlBase)
 
